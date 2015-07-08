49bd903c2ec4cee23d7a50e44a153fe6971234b6
Daniel <dan_pc@live.com>
2015-07-08 17:03:36 -0400

    Added dbhelper functions

--- backend/dbhelper.js
+++ backend/dbhelper.js
@@ -1,12 +1,39 @@
 var fs = require('fs');
 var mysql = require('mysql');
+var express = require('express');
+var app = express();
 
-connect();
+var dbhelper = {
+    connection : null,
 
-function connect() {
-    var connection = mysql.createConnection(JSON.parse(fs.readFileSync("credentials.json", 'utf8')));
+    connect: function () {
+        connection = mysql.createConnection(JSON.parse(fs.readFileSync('credentials.json', 'utf8')));
 
-    connection.connect(function(err) {
-        console.log("error: " + err);
-    });
+        connection.connect(function(err) {
+            console.log('error: ' + err);
+        });
+    },
+
+    signIn: function(user) {
+        connection.query('INSERT INTO Users SET ?', {username: connection.escape(user)}, function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            console.log('result: ' + result);
+        });
+    },
+
+    signOut: function(user) {
+        connection.query('DELETE FROM Users WHERE username = "' + connection.escape(user) + '"', function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            console.log('result' + result);
+        });
+    }
 }
+
+dbhelper.connect();
+dbhelper.signOut('daniel');
