1dafbb9e2b28088e730cdf62732e628c21602be2
Daniel <dan_pc@live.com>
2015-07-08 23:20:05 -0400

    project structure fixed

---
+++ backend/dbhelper.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var mysql = require('mysql');
+var express = require('express');
+var socketio = require('socket.io')(80);
+
+var app = express();
+
+var dbhelper = {
+    connection : null,
+
+    connect: function () {
+        connection = mysql.createConnection(JSON.parse(fs.readFileSync('credentials.json', 'utf8')));
+
+        connection.connect(function(err) {
+            console.log('error: ' + err);
+        });
+    },
+
+    signIn: function(user) {
+        connection.query('INSERT INTO Users SET ?', {username: connection.escape(user)}, function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            console.log('result: ' + result);
+        });
+    },
+
+    signOut: function(user) {
+        connection.query('DELETE FROM Users WHERE username = "' + connection.escape(user) + '"', function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            console.log('result' + result);
+        });
+    }
+}
